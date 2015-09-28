66db73b26eaf4f0d6f868974e7b3cd8b39e79ac3
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2015-09-28 17:28:59 -0500

    go

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             <?php
             	$current_url = base64_encode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
                 
-            	$results = $mysqli->query("SELECT * FROM products ORDER BY product_code DESC");
+            	$results = $mysqli->query("SELECT * FROM products ORDER BY product_code ASC");
             	
                 if ($results) { 
             	
