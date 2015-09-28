b8b3c9e7d05bc48c4bbb27784fd658c47c9ca2fc
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2015-09-28 14:45:22 -0500

    go

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@ $currency = '$'; //Currency sumbol or code
 
 $db_username = 'peach1';
 $db_password = 'peach';
-$db_name = 'peach_press';
+$db_name = 'peach';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
