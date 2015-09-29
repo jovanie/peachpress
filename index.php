da2d6ccf260079b254951ad5c4ea0e858bcddfcc
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2015-09-29 13:51:19 -0500

    go

--- index.php
+++ index.php
@@ -8,26 +8,24 @@
             	$results = $mysqli->query("SELECT * FROM products ORDER BY product_code ASC");
             	
                 if ($results) { 
-            	
-                    //fetch results set as object and output HTML
                     while($obj = $results->fetch_object())
                     {
-            			echo '<li>';
-            			echo '<form method="post" action="php/cart_update.php">';
-            			echo '<div class="product-thumb"><img src="img/products/'.$obj->product_code.'.jpg"></div>';
-                        echo '<h3>'.$obj->product_name.'</h3>';
-                        echo '<div class="product-desc">'.$obj->product_desc.'</div>';
-                        echo '<div class="product-info">';
-            			echo '<p>&nbsp;</p>Price '.$currency.$obj->price.' | ';
-            			echo '<button class="add_to_cart">Add To Cart</button>';
-            			echo '</div>';
+                        echo '<div class="productItem">';
+                        echo '<li><form method="post" action="php/cart_update.php">';
+                        echo '<a href="#">';
+            			echo '<div class="productImg"><img src="img/products/'.$obj->product_code.'.jpg"></div>';
+                        echo '<h3>'.$obj->product_name.'</h3></a>';
+                            echo '<div class="productDesc">'.$obj->product_desc.'</div>';
+                            echo '<div class="productInfo">';
+                            echo '<p>&nbsp;</p>Price '.$currency.$obj->price.' | ';
+                            echo '<button class="add_to_cart">Add To Cart</button>';
+                            echo '</div>';
                         echo '<input type="hidden" name="product_code" value="'.$obj->product_code.'" />';
                         echo '<input type="hidden" name="type" value="add" />';
             			echo '<input type="hidden" name="return_url" value="'.$current_url.'" />';
-                        echo '</form>';
-                        echo '</li>';
+                        echo '</form></li>';
+                        echo '</div>';
                     }
-                
                 }
             ?>    			
     	</ul>
