8355d46fe69b1553ffac36ab787b149114c19065
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2015-09-29 14:36:55 -0500

    go

--- index.php
+++ index.php
@@ -29,8 +29,7 @@
                 if ($results) { 
                     while($obj = $results->fetch_object())
                     {
-                        echo '<div class="productItem">';
-                        echo '<li><form method="post" action="php/cart_update.php">';
+                        echo '<li class="productItem"><form method="post" action="php/cart_update.php">';
                         echo '<a href="#">';
             			echo '<div class="productImg"><img src="img/products/'.$obj->product_code.'.jpg"></div>';
                         echo '<h3>'.$obj->product_name.'</h3>';
@@ -43,7 +42,6 @@
                         echo '<input type="hidden" name="type" value="add" />';
             			echo '<input type="hidden" name="return_url" value="'.$current_url.'" />';
                         echo '</a></form></li>';
-                        echo '</div>';
                     }
                 }
             ?>    			
